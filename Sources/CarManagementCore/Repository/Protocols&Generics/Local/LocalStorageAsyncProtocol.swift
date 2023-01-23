ccd3431f3fc057ce90b1f88a2d19a93833f2d4ba
eldin <eldinsmakic@hotmail.fr>
2023-01-23 22:23:41 +0100

    [Refacto]: Set localStorageAsyncProtocol to its own file

---
+++ Sources/CarManagementCore/Repository/Protocols&Generics/Local/LocalStorageAsyncProtocol.swift
@@ -0,0 +1,25 @@
+//
+//  LocalStorageProtocolAsync.swift
+//  
+//
+//  Created by eldin smakic on 23/01/2023.
+//
+
+import Foundation
+
+public protocol StorageProtocolAsync {
+    associatedtype Value: Equatable
+
+    func add(_ value: Value) async throws -> Value
+    func update(_ value: Value) async throws -> Value
+    func remove(_ value: Value) async throws -> Value
+    func fetch() async throws  -> [Value]
+    func erase() async throws
+}
+
+public enum GenericErrorAsync: Error {
+    case unknowError
+}
+
+public protocol LocalStorageProtocolAsync: StorageProtocolAsync {}
+public protocol RemoteStorageProtocolAsync: StorageProtocolAsync {}
--- Sources/CarManagementCore/Repository/Voiture/Storage/VoitureLocalStorageAsync.swift
+++ Sources/CarManagementCore/Repository/Voiture/Storage/VoitureLocalStorageAsync.swift
@@ -8,23 +8,6 @@
 import Foundation
 import Defaults
 
-public protocol StorageProtocolAsync {
-    associatedtype Value: Equatable
-
-    func add(_ value: Value) async throws -> Value
-    func update(_ value: Value) async throws -> Value
-    func remove(_ value: Value) async throws -> Value
-    func fetch() async throws  -> [Value]
-    func erase() async throws
-}
-
-public enum GenericErrorAsync: Error {
-    case unknowError
-}
-
-public protocol LocalStorageProtocolAsync: StorageProtocolAsync {}
-public protocol RemoteStorageProtocolAsync: StorageProtocolAsync {}
-
 public class VoitureLocalStorageAsync: LocalStorageProtocolAsync {
     public static var shared = VoitureLocalStorageAsync()
 
