c54459bab8fd80c9088eb71e89ac466ef6f296bc
eldin <eldinsmakic@hotmail.fr>
2023-05-13 22:43:00 +0200

    [Feat]: Create OperationRepository and create separet file for localStorage

--- Sources/CarManagementCore/Repository/Operation/OperationRepositoryProtocol.swift
+++ Sources/CarManagementCore/Repository/Operation/OperationRepositoryProtocol.swift
@@ -6,21 +6,6 @@
 //
 
 import Foundation
-import RealmSwift
-
-public protocol OperationStorageProtocol {
-    func create(withCarId carId: UUID) async throws -> OperationDTO
-    func add(_ value: OperationDTO) async -> Result<OperationDTO,GenericErrorAsync>
-    func update(_ value: OperationDTO) async -> Result<OperationDTO,GenericErrorAsync>
-    func remove(_ value: OperationDTO) async -> Result<OperationDTO,GenericErrorAsync>
-    func remove(byCarID carId: UUID, andId id: UUID) async throws -> OperationDTO
-    func get(fromCarId carId: UUID, andId id: UUID) async -> Result<OperationDTO,GenericErrorAsync>
-    func fetchAllMixed() async -> [OperationDTO]
-    func fetch() async throws -> [UUID:[OperationDTO]]
-    func fetchLastOperations() async throws -> [UUID:[OperationDTO]]
-    func fetch(byCarId carID: UUID) async throws -> [OperationDTO]
-    func erase() async -> Result<Void,GenericErrorAsync>
-}
 
 public protocol OperationRepositoryProtocol: OperationStorageProtocol {}
 
@@ -28,166 +13,52 @@ public protocol OperationRepositoryProtocol: OperationStorageProtocol {}
     public static var shared = BackgroundActor()
 }
 
-public final class OperationLocaleStorageRealm: OperationStorageProtocol {
-    private let realm: Realm
+public final class OperationRepository: OperationRepositoryProtocol {
+    @Injected var localStorage: OperationStorageProtocol
+    
+    public init() {}
 
-
-    public init() async throws {
-        realm = try await Realm(configuration: Realm.Configuration(deleteRealmIfMigrationNeeded: true),actor: BackgroundActor.shared)
-    }
-
-    @BackgroundActor
     public func create(withCarId carId: UUID) async throws -> OperationDTO {
-        let operationLocale = OperationLocaleEntity(carId: carId)
-        do {
-            return try await self.add(operationLocale.toDTO()).get()
-        } catch {
-            throw error
-        }
+        try await localStorage.create(withCarId: carId)
     }
-
-    @BackgroundActor
+    
     public func add(_ value: OperationDTO) async -> Result<OperationDTO, GenericErrorAsync> {
-        do {
-            let operationLocale = OperationLocaleEntity(operationDTO: value)
-            try await realm.asyncWrite {
-                self.realm.add(operationLocale, update: .modified)
-            }
-            return .success(operationLocale.toDTO())
-        } catch {
-            return .failure(.unknowError)
-        }
+        await localStorage.add(value)
     }
     
     public func update(_ value: OperationDTO) async -> Result<OperationDTO, GenericErrorAsync> {
-        await add(value)
+        await localStorage.update(value)
     }
-
-    @BackgroundActor
+    
     public func remove(_ value: OperationDTO) async -> Result<OperationDTO, GenericErrorAsync> {
-        let entity = OperationLocaleEntity(operationDTO: value)
-        do {
-            let all = realm.objects(OperationLocaleEntity.self)
-            let result = all.filter { operation in
-                operation.carId == entity.carId &&
-                operation.id == entity.id
-            }
-            try realm.write {
-                realm.delete(result)
-            }
-            return .success(value)
-        } catch {
-            return .failure(.unknowError)
-        }
+        await localStorage.remove(value)
     }
     
     public func remove(byCarID carId: UUID, andId id: UUID) async throws -> OperationDTO {
-        .init(carId: .init(), id: .init(), title: "", mileage: 0, cost: 0, date: .now, type: .maintenance)
+        try await localStorage.remove(byCarID: carId, andId: id)
     }
     
     public func get(fromCarId carId: UUID, andId id: UUID) async -> Result<OperationDTO, GenericErrorAsync> {
-        .failure(.unknowError)
+        await localStorage.get(fromCarId: carId, andId: id)
     }
-
-    @BackgroundActor
+    
     public func fetchAllMixed() async -> [OperationDTO] {
-        let all = realm.objects(OperationLocaleEntity.self)
-        return all.map { $0.toDTO() }
+        await localStorage.fetchAllMixed()
     }
     
     public func fetch() async throws -> [UUID : [OperationDTO]] {
-        var result: [UUID: [OperationDTO]] = [:]
-        let all = realm.objects(OperationLocaleEntity.self)
-
-        let carIds = Set(all.map { $0.carId })
-        for carId in carIds {
-            result[carId] = all.where { $0.carId == carId }.map { $0.toDTO() }
-        }
-        return result
+        try await localStorage.fetch()
     }
     
     public func fetchLastOperations() async throws -> [UUID : [OperationDTO]] {
-        var result: [UUID: [OperationDTO]] = [:]
-        let all = realm.objects(OperationLocaleEntity.self).sorted(by: \.date)
-    
-        let carIds = Set(all.map { $0.carId })
-        for carId in carIds {
-            let operations = all.where { $0.carId == carId }.suffix(5).map { $0.toDTO() }
-            result[carId] = Array(operations)
-        }
-        return result
+        try await localStorage.fetchLastOperations()
     }
     
     public func fetch(byCarId carID: UUID) async throws -> [OperationDTO] {
-        let all = realm.objects(OperationLocaleEntity.self).sorted(by: \.date)
-        return all.where { $0.carId == carID }.map { $0.toDTO() }
+        try await localStorage.fetch(byCarId: carID)
     }
-
-    @BackgroundActor
+    
     public func erase() async -> Result<Void, GenericErrorAsync> {
-        do {
-            try realm.write { [self] in
-                let all = self.realm.objects(OperationLocaleEntity.self)
-                realm.delete(all)
-            }
-            return .success(())
-        } catch {
-            return .failure(.unknowError)
-        }
+        await localStorage.erase()
     }
 }
-
-public class OperationLocaleEntity: Object {
-    @Persisted var carId: UUID
-    @Persisted(primaryKey: true) var id: UUID
-    @Persisted var title: String
-    @Persisted var mileage: Int
-    @Persisted var cost: Double
-    @Persisted var date: Date
-    @Persisted var type: String
-
-    convenience init(carId: UUID, title: String, mileage: Int, cost: Double, date: Date, type: String) {
-        self.init()
-        self.carId = carId
-        self.title = title
-        self.mileage = mileage
-        self.cost = cost
-        self.date = date
-        self.type = type
-    }
-
-    convenience init(carId: UUID) {
-        self.init()
-        self.carId = carId
-        self.title = ""
-        self.mileage = 0
-        self.cost = 0
-        self.date = .now
-        self.type = OperationTypeDTO.maintenance.rawValue
-    }
-
-    convenience init(operationDTO: OperationDTO) {
-        self.init()
-        self.carId = operationDTO.carId
-        self.id = operationDTO.id
-        self.title = operationDTO.title
-        self.mileage = operationDTO.mileage
-        self.cost = operationDTO.cost
-        self.date = operationDTO.date
-        self.type = operationDTO.type.rawValue
-    }
-
-    func toDTO() -> OperationDTO {
-        OperationDTO(
-            carId: carId,
-            id: id,
-            title: title,
-            mileage: mileage,
-            cost: cost,
-            date: date,
-            type: .init(rawValue: type) ?? .maintenance
-        )
-    }
-}
-
-
--- Tests/CarManagementCoreTests/Repository/Operations/TestsOperationRepository.swift
+++ Tests/CarManagementCoreTests/Repository/Operations/TestsOperationRepository.swift
@@ -1,222 +1,226 @@
 //
-//  TestsOperationsLocalStorageRealm.swift
+//  TestsOperationRepository.swift
 //  
 //
-//  Created by eldin smakic on 12/05/2023.
+//  Created by eldin smakic on 13/05/2023.
 //
 
 import XCTest
 import CarManagementCore
 
-final class TestsOperationsLocalStorageRealm: XCTestCase {
-    var localStorage: OperationStorageProtocol!
+final class TestsOperationRepository: XCTestCase {
+    var repository: OperationRepositoryProtocol!
     let firstCarId: UUID = .init()
     let secondCarId: UUID = .init()
-
-    func createLocalStorage() async -> OperationStorageProtocol { try! await OperationLocaleStorageRealm() }
-
+    
+    func createRepository() async -> OperationRepositoryProtocol {  OperationRepository() }
+    
     func createValue() -> OperationDTO { .init(carId: firstCarId, id: .init(), title: "title1", mileage: 1, cost: 1, date: .now, type: .maintenance) }
     func createSecondValue() -> OperationDTO { .init(carId: firstCarId, id: .init(), title: "title2", mileage: 2, cost: 2, date: .now, type: .repair) }
     func createValueSecondCar() -> OperationDTO {
         FakeData.Operation.create(carId: secondCarId)
     }
-
+    
     func createSecondValueSecondCar() -> OperationDTO {
         FakeData.Operation.create(carId: secondCarId)
     }
-
+    
     public override func setUp() async throws {
         try await super.setUp()
-        localStorage = await createLocalStorage()
-        _ = await self.localStorage.erase()
+        let localStorage = try await OperationLocaleStorageRealm()
+        Injection.shared.register(OperationStorageProtocol.self) { _ in
+            localStorage
+        }
+        repository = await createRepository()
+        _ = await self.repository.erase()
     }
-
+    
     // MARK: - Get
     func test_get_expect_zero() async throws {
-        let values = await localStorage.fetchAllMixed()
+        let values = await repository.fetchAllMixed()
         // Asserting that our Combine pipeline yielded the
         // correct output:
         XCTAssertEqual(values.count, 0)
     }
-
+    
     // MARK: - ADD
     func test_add_two_expect_two_same_value() async throws {
         var input = createValue()
         var secondInput = createSecondValue()
-
-        input = try await localStorage.add(input).get()
-        secondInput = try await localStorage.add(secondInput).get()
-
-        let values = await localStorage.fetchAllMixed()
-
+        
+        input = try await repository.add(input).get()
+        secondInput = try await repository.add(secondInput).get()
+        
+        let values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values.count, 2)
         XCTAssertEqual(values, [input, secondInput])
     }
-
+    
     func test_add_one_expect_one() async throws {
         let value = createValue()
-
-        _ = await localStorage.add(value)
-        let values = await localStorage.fetchAllMixed()
-
+        
+        _ = await repository.add(value)
+        let values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values.count, 1)
     }
-
+    
     func test_add_two_expect_two() async throws {
         let value = createValue()
         let secondValue = createSecondValue()
-        let _ = await localStorage.add(value)
-        _ = await localStorage.add(secondValue)
+        let _ = await repository.add(value)
+        _ = await repository.add(secondValue)
         
-        let values = await localStorage.fetchAllMixed()
-
+        let values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values.count, 2)
     }
-
+    
     func test_add_When_add_three_with_same_value_expect_One() async throws {
         let value = createValue()
-
-        _ = try await localStorage.add(value).get()
-        _ = try await localStorage.add(value).get()
-        _ = try await localStorage.add(value).get()
         
-        let values = await localStorage.fetchAllMixed()
-
+        _ = try await repository.add(value).get()
+        _ = try await repository.add(value).get()
+        _ = try await repository.add(value).get()
+        
+        let values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values.count, 1)
     }
-
+    
     func test_add_three_expect_three() async throws {
         let value = createValue()
         let secondValue = createSecondValue()
         let thirdValue = createValueSecondCar()
-
-        _ = try await localStorage.add(value).get()
-        _ = try await localStorage.add(secondValue).get()
-        _ = try await localStorage.add(thirdValue).get()
         
-        let values = await localStorage.fetchAllMixed()
-
+        _ = try await repository.add(value).get()
+        _ = try await repository.add(secondValue).get()
+        _ = try await repository.add(thirdValue).get()
+        
+        let values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values.count, 3)
     }
-
+    
     // MARK: - Remove
-
+    
     func test_add_one_remove_one_expect_zero() async throws {
-        let value = try await localStorage.add(createValue()).get()
-
-        var values = await localStorage.fetchAllMixed()
+        let value = try await repository.add(createValue()).get()
+        
+        var values = await repository.fetchAllMixed()
         
         XCTAssertEqual(values.count, 1)
-
-         _ = await localStorage.remove(value)
-
-        values = await localStorage.fetchAllMixed()
-
+        
+        _ = await repository.remove(value)
+        
+        values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values.count, 0)
     }
-
+    
     func test_remove_when_add_two_remove_one_expect_one() async throws {
-        let value = try await localStorage.add(createValue()).get()
-        let secondValue =  try await localStorage.add(createSecondValue()).get()
-        _ = await localStorage.remove(value)
-
-        let values = await localStorage.fetchAllMixed()
-
+        let value = try await repository.add(createValue()).get()
+        let secondValue =  try await repository.add(createSecondValue()).get()
+        _ = await repository.remove(value)
+        
+        let values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values, [secondValue])
     }
-
+    
     // MARK: - Erase
-
+    
     func test_erase_when_add_two_erase_expect_zero() async throws {
         let value = createValue()
-
-        _ = await localStorage.add(value)
-        _ = await localStorage.add(value)
-
-        _ = await localStorage.erase()
- 
-        let values = await localStorage.fetchAllMixed()
-
+        
+        _ = await repository.add(value)
+        _ = await repository.add(value)
+        
+        _ = await repository.erase()
+        
+        let values = await repository.fetchAllMixed()
+        
         XCTAssertEqual(values.count, 0)
     }
-
+    
     // MARK: - Fetch
-
+    
     func test_fetch_When_no_data_expect_zero() async throws {
-        let result = try await localStorage.fetch()
-
+        let result = try await repository.fetch()
+        
         XCTAssertEqual(result.count, 0)
     }
-
+    
     func test_fetch_When_add_one_or_more_car_operation_from_one_car_expect_one() async throws {
-        _ = await localStorage.add(createValue())
-        _ = await localStorage.add(createSecondValue())
-        let result = try await localStorage.fetch()
+        _ = await repository.add(createValue())
+        _ = await repository.add(createSecondValue())
+        let result = try await repository.fetch()
         
         XCTAssertEqual(result.count, 1)
     }
     
     func test_fetch_When_add_two_car_operation_expect_two() async throws {
         
-        _ = await localStorage.add(createValue())
-        _ = await localStorage.add(createSecondValue())
-        _ = await localStorage.add(createValueSecondCar())
-        _ = await localStorage.add(createSecondValueSecondCar())
-        let result = try await localStorage.fetch()
+        _ = await repository.add(createValue())
+        _ = await repository.add(createSecondValue())
+        _ = await repository.add(createValueSecondCar())
+        _ = await repository.add(createSecondValueSecondCar())
+        let result = try await repository.fetch()
         
         XCTAssertEqual(result.count, 2)
     }
-
+    
     // MARK: - Create
-
+    
     func test_create_When_create_expect_one() async throws {
-        let value = try await localStorage.create(withCarId: firstCarId)
-
-        let result = await localStorage.fetchAllMixed()
-
+        let value = try await repository.create(withCarId: firstCarId)
+        
+        let result = await repository.fetchAllMixed()
+        
         XCTAssertEqual(result.count, 1)
     }
-
-
+    
+    
     // MARK: - fetchLastOperations
-
+    
     func test_fetchLastOperations_When_no_data_expect_empty() async throws {
-        let fetch = try await localStorage.fetchLastOperations()
-
+        let fetch = try await repository.fetchLastOperations()
+        
         XCTAssertEqual(fetch.isEmpty, true)
     }
-
+    
     func test_fetchLastOperations_When_add_value_expect_last_five() async throws {
-
+        
         for value in (1...10) {
-             _ = await localStorage.add(FakeData.Operation.create(carId: firstCarId, title: "\(value)"))
+            _ = await repository.add(FakeData.Operation.create(carId: firstCarId, title: "\(value)"))
         }
-
-        let fetch = try await localStorage.fetchLastOperations()
-
+        
+        let fetch = try await repository.fetchLastOperations()
+        
         XCTAssertEqual(fetch.count, 1)
         XCTAssertEqual(fetch[firstCarId]?.count, 5)
-
+        
         var index = 6
         for value in fetch[firstCarId] ?? [] {
             XCTAssertEqual(value.title, "\(index)")
             index += 1
         }
     }
-
+    
     func test_fetchLastOperations_When_add_2_type_of_car_value_expect_last_five() async throws {
-
+        
         for value in (1...15) {
-            _ = await localStorage.add(FakeData.Operation.create(carId: firstCarId, title: "\(firstCarId)|\(value)"))
-            _ = await localStorage.add(FakeData.Operation.create(carId: secondCarId, title: "\(secondCarId)|\(value)"))
+            _ = await repository.add(FakeData.Operation.create(carId: firstCarId, title: "\(firstCarId)|\(value)"))
+            _ = await repository.add(FakeData.Operation.create(carId: secondCarId, title: "\(secondCarId)|\(value)"))
         }
-
-        let fetch = try await localStorage.fetchLastOperations()
-
+        
+        let fetch = try await repository.fetchLastOperations()
+        
         XCTAssertEqual(fetch.count, 2)
         XCTAssertEqual(fetch[firstCarId]?.count, 5)
         XCTAssertEqual(fetch[secondCarId]?.count, 5)
-
+        
         var index = 11
         for (key, operations) in fetch {
             for operation in operations {
@@ -226,27 +230,24 @@ final class TestsOperationsLocalStorageRealm: XCTestCase {
             index = 11
         }
     }
-
+    
     // MARK: - Fetch(ByCarId)
     
     func test_fetchByCarId_When_No_value_Expect_empty() async throws {
-        let result = try await localStorage.fetch(byCarId: firstCarId)
+        let result = try await repository.fetch(byCarId: firstCarId)
         
         XCTAssertEqual(result.count, 0)
     }
-
+    
     func test_fetchByCarId_When_two_value_Expect_two() async throws {
-        _ = await localStorage.add(FakeData.Operation.create(carId: firstCarId))
-        _ = await localStorage.add(FakeData.Operation.create(carId: firstCarId))
-
-        _ = await localStorage.add(FakeData.Operation.create(carId: secondCarId))
-        _ = await localStorage.add(FakeData.Operation.create(carId: secondCarId))
-        _ = await localStorage.add(FakeData.Operation.create(carId: secondCarId))
-        let result = try await localStorage.fetch(byCarId: firstCarId)
+        _ = await repository.add(FakeData.Operation.create(carId: firstCarId))
+        _ = await repository.add(FakeData.Operation.create(carId: firstCarId))
+        
+        _ = await repository.add(FakeData.Operation.create(carId: secondCarId))
+        _ = await repository.add(FakeData.Operation.create(carId: secondCarId))
+        _ = await repository.add(FakeData.Operation.create(carId: secondCarId))
+        let result = try await repository.fetch(byCarId: firstCarId)
         
         XCTAssertEqual(result.count, 2)
     }
 }
-
-
-
